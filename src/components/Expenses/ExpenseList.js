a7b3099565927aa4893aedbb8b06235d4cca303e
Sanket Khardekar <khardekarsanket@gmail.com>
2022-01-21 17:03:25 +0530

    Added ExpenseList Component
    This will render all the expenses.

---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,9 @@
+import React from "react";
+const ExpenseList =(props)=>{
+    return(
+        <ul className="expenses-list">
+            Mapping Expenses with ExpenseItem
+        </ul>
+    );
+}
+export default ExpenseList;
