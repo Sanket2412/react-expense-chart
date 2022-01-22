fee5dfa245c42436bcabb1d1426254e58444fbe3
Sanket Khardekar <khardekarsanket@gmail.com>
2022-01-22 17:23:55 +0530

    Created Chart and Expense Chart Component
    Created Chart and Expense Chart Component.

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Chart.css";
+
+const Chart=(props)=>{
+    return(
+        <div className="chart">
+            ChartBar Component...
+        </div>
+    );
+}
+export default Chart;
